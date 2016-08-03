78c38a6ca4d770520a8c57e80630de8516b7a851
H Hugo Falkman <zilla@hs-macbook-pro.local>
2016-08-03 17:06:32 +0200

    Initial Commit

---
+++ TestLiterals/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TestLiterals
+//
+//  Created by H Hugo Falkman on 2016-08-03.
+//  Copyright © 2016 H Hugo Falkman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TestLiterals/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestLiterals
+//
+//  Created by H Hugo Falkman on 2016-08-03.
+//  Copyright © 2016 H Hugo Falkman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
